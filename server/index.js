52cfa2b7a53bfcf217f571effdda4b3f402a7109
teslasdev <Teslasdeveloper@gmail.com>
2023-06-02 00:00:52 +0100

    okkk

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const {Configuration, OpenAIApi} = require("openai");
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
+const { OpenAI } = require("langchain/llms/openai");
+const  { BufferMemory } = require("langchain/memory");
+const { ConversationChain } = require("langchain/chains");
+const {
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+  } = require("langchain/prompts");
+const chat = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY , temperature: 0 , maxTokens: 1000 });
 app.use(cors());
 app.use(express.json());
 app.use('/', express.static(__dirname + '/frontend')); // Serves resources from client folder
@@ -27,39 +31,22 @@ app.post('/get-prompt-result', async (req, res) => {
     }
 
     try {
-        // Use the OpenAI SDK to create a completion
-        // with the given prompt, model and maximum tokens
-        if (model === 'image') {
-            const result = await openai.createImage({
-                prompt,
-                response_format: 'url',
-                size: '512x512'
-            });
-            return res.send(result.data.data[0].url);
-        }
-        const result = await openai.createChatCompletion({
-            model: model === 'gpt' ? "gpt-3.5-turbo" : 'code-davinci-002', // model name
-            messages: [
-                { role: "user", content: prompt }
-            ],
-            temperature: 0, // Higher values means the model will take more risks.
-            max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-            top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-            frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-            presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-        })
-        console.log(result.data.choices);
-        return res.send(result.data.choices[0]?.message?.content);
-        // const completion = await openai.createCompletion({
-        //     model: model === 'gpt' ? "text-davinci-003" : 'code-davinci-002', // model name
-        //     prompt: `${prompt}`,
-        //     temperature: 0, // Higher values means the model will take more risks.
-        //     max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-        //     top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-        //     frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-        //     presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-        // });
-        // Send the generated text as the response
+        const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+            new MessagesPlaceholder("history"),
+            HumanMessagePromptTemplate.fromTemplate("{input}"),
+          ]);
+          
+          const chain = new ConversationChain({
+            memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+            prompt: chatPrompt,
+            llm: chat,
+          });
+
+          const responseH = await chain.call({
+            input: prompt
+          });
+          
+           return res.send(responseH.response);
     } catch (error) {
         const errorMsg = error.response ? error.response.data.error : `${error}`;
         console.error(errorMsg);
